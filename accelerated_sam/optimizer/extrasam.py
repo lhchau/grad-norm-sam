16e4a7bd003090074fdf175f9f13fbccede8185e
lhchau <lhchau20@apcs.fitus.edu.vn>
2024-05-04 15:51:45 +0700

    variant

--- accelerated_sam/optimizer/extrasam.py
+++ accelerated_sam/optimizer/extrasam.py
@@ -29,9 +29,9 @@ class EXTRASAM(torch.optim.Optimizer):
                 
                 p.add_(param_state['step_length'])
                 
-                # if 'exp_avg_old_g' not in param_state:
-                #     param_state['exp_avg_old_g'] = torch.zeros_like(p, memory_format=torch.preserve_format)
-                # param_state['exp_avg_old_g'].lerp_(p.grad, 1-self.beta)
+                if 'exp_avg_old_g' not in param_state:
+                    param_state['exp_avg_old_g'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                param_state['exp_avg_old_g'].lerp_(p.grad, 1-self.beta)
                 
                 e_w = (torch.pow(p, 2) if group["adaptive"] else 1.0) * p.grad * scale.to(p)
                 p.add_(e_w)  # climb to the local maximum "w + e(w)"
@@ -42,7 +42,7 @@ class EXTRASAM(torch.optim.Optimizer):
     @torch.no_grad()
     def second_step(self, zero_grad=False):
         # self.new_grad_norm = self._grad_norm()
-        # self.exp_avg_old_grad_norm = self._grad_norm(by='exp_avg_old_g')
+        self.exp_avg_old_grad_norm = self._grad_norm(by='exp_avg_old_g')
         for group in self.param_groups:
             weight_decay = group["weight_decay"]
             step_size = group['lr']
@@ -57,8 +57,8 @@ class EXTRASAM(torch.optim.Optimizer):
                 d_p = p.grad.data
                 
                 # param_state['step_length'] = d_p.mul(-step_size/self.new_grad_norm)
-                param_state['step_length'] = param_state["e_w"].mul(-1)
-                # param_state['step_length'] = param_state['exp_avg_old_g'].mul(-rho/self.exp_avg_old_grad_norm)
+                # param_state['step_length'] = param_state["e_w"].mul(-1)
+                param_state['step_length'] = param_state['exp_avg_old_g'].mul(-rho/self.exp_avg_old_grad_norm)
                 
                 if weight_decay != 0:
                     d_p.add_(p.data, alpha=weight_decay)
